dec4cb63d6ce5bd3475670c65ea324f28b012b6a
DaneilKung <95456170+DaneilKung@users.noreply.github.com>
2022-01-04 14:19:20 +0800

    Create JavaScriptBasic.js

---
+++ Week-1/Assignment-3/JavaScriptBasic.js
@@ -0,0 +1,22 @@
+function countAandB(input) {
+  
+  
+}
+
+function toNumber(input) {
+  
+  
+}
+
+let input1 = [];
+
+console.log(countAandB(input1));
+
+console.log(toNumber(input1));
+
+let input2 = [];
+
+console.log(countAandB(input2));
+
+console.log(toNumber(input2));
+
