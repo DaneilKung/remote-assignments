54a447b7c0ff6adec0aed327261efd69f800fdcb
DaneilKung <95456170+DaneilKung@users.noreply.github.com>
2022-01-04 14:58:40 +0800

    Update JavaScriptBasic.js

--- Week-1/Assignment-3/JavaScriptBasic.js
+++ Week-1/Assignment-3/JavaScriptBasic.js
@@ -8,13 +8,13 @@ function toNumber(input) {
   
 }
 
-let input1 = [];
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
 
 console.log(countAandB(input1));
 
 console.log(toNumber(input1));
 
-let input2 = [];
+let input2 = ['e', 'd', 'c', 'd', 'e'];
 
 console.log(countAandB(input2));
 
